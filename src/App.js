175b421ab864976f76cb34115b32c9be0441e785
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-04-11 01:06:30 +0900

    create functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   // const greeting = 'Hi, Tom!';
   // return <h1 className="foo">{greeting}</h1>;
   return (
@@ -14,6 +14,6 @@ function App() {
       ></input>
     </React.Fragment>
   );
-}
+};
 
 export default App;
